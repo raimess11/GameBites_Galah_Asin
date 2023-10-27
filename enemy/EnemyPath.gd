49545d06d6d462e89de4db76e24dca6535264394
Fakhrul0121 <106474317+Fakhrul0121@users.noreply.github.com>
2023-10-27 14:44:04 +0700

    final update (hopefully)

--- enemy/EnemyPath.gd
+++ enemy/EnemyPath.gd
@@ -2,20 +2,22 @@ extends Path2D
 
 @export var player: Player
 @export var speed = 0.01
+@export var follow_barrier = 0
 
 @onready var PathFollow = $PathFollow2D
 
 var max_height
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	max_height = countHeight()
-	print(max_height)
+	max_height = countHeight() + position.y
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if player.position.x > max_height:
+	pass
+	if player.position.y < (max_height - follow_barrier) or player.position.y > (max_height + follow_barrier):
 		PathFollow.set_physics_process(false)
 	else:
 		PathFollow.set_physics_process(true)
@@ -23,6 +25,6 @@ func _process(delta):
 func countHeight() -> int:
 	var max = 0
 	for vector in curve.get_baked_points():
-		if vector.x < max:
-			max = vector.x
+		if vector.y < max:
+			max = vector.y
 	return max
